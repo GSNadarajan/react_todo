27a84134f8d1a1535fcf89cb3ad6141fc90a3559
NadarajanGS <nadarajangs31@gmail.com>
2023-06-30 00:55:16 +0530

    search completed with add task

--- src/App.js
+++ src/App.js
@@ -1,50 +1,49 @@
-import './App.css';
 import { useState } from 'react';
 import Header from './components/Header';
 import Todo from './components/Todo';
 import Footer from './components/Footer';
 
 function App() {
-  // use state for maintaining the tasks
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem("todolist")) || []);
+      // use state for maintaining the tasks
+      const [items, setItems] = useState(JSON.parse(localStorage.getItem("todolist")) || []);
 
-  // To delete the task corresponding to the id
-  function handleDelete(id) {
-    alert('Are you sure you want to delete this task?');
-    const updatedItems = items.filter(item => item.id !== id);
-    setItems(updatedItems);
-    localStorage.setItem("todolist", JSON.stringify(updatedItems));
-  }
+      // To delete the task corresponding to the id
+      function handleDelete(id) {
+        alert('Are you sure you want to delete this task?');
+        const updatedItems = items.filter(item => item.id !== id);
+        setItems(updatedItems);
+        localStorage.setItem("todolist", JSON.stringify(updatedItems));
+      }
 
-  // To check the task whether it is completed or not
-  function handleCheck(id) {
-    const updatedItems = items.map(item => item.id === id ? { ...item, checked: !item.checked } : item);
-    setItems(updatedItems);
-    localStorage.setItem("todolist", JSON.stringify(updatedItems));
-  }
+      // To check the task whether it is completed or not
+      function handleCheck(id) {
+        const updatedItems = items.map(item => item.id === id ? { ...item, checked: !item.checked } : item);
+        setItems(updatedItems);
+        localStorage.setItem("todolist", JSON.stringify(updatedItems));
+      }
 
-  // use state for maintaining the new task to be added
-  const [newItem, setNewItem] = useState('');
+      // use state for maintaining the new task to be added
+      const [newItem, setNewItem] = useState('');
 
-  // To add new task with the existing state
-  function handleSubmit(e) {
-    e.preventDefault();
-    addItem(newItem);
-    setNewItem('');
-  }
+      // To add new task with the existing state
+      function handleSubmit(e) {
+        e.preventDefault();
+        addItem(newItem);
+        setNewItem('');
+      }
 
-  function addItem(task) {
-    const id = items.length ? items[items.length - 1].id + 1 : 1;
-    const addItems = { id, task, checked: false };
-    const updatedItems = [...items, addItems];
-    setItems(updatedItems);
-    localStorage.setItem("todolist", JSON.stringify(updatedItems));
-  }
+      function addItem(task) {
+        const id = items.length ? items[items.length - 1].id + 1 : 1;
+        const addItems = { id, task, checked: false };
+        const updatedItems = [...items, addItems];
+        setItems(updatedItems);
+        localStorage.setItem("todolist", JSON.stringify(updatedItems));
+      }
 
-  // use state for maintaining the search item
-  const [searchItem, setSearchItem] = useState('');
+      // use state for maintaining the search item
+      const [searchItem, setSearchItem] = useState('');
 
-  return (
+return (
     <div className="app">
       <Header />
       <Todo
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,10 @@ import React from "react";
 const Footer =() => {
     return(
 
-        <div className="py-4">
-            <p className="text-center ">Made with love by Nadarajan G S </p>
+        <div className="fixed bottom-0 left-0 right-0 flex items-center justify-center bg-blue-400 p-4" >
+            <p className="text-center text-white text-1xl">Made with love by Nadarajan G S </p>
+
+            
         </div>
 
 
