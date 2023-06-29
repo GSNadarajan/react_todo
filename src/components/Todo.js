70087e01352d217034a46044b4d7bd61c246d10e
NadarajanGS <nadarajangs31@gmail.com>
2023-06-29 13:11:46 +0530

    add tasks finished

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,45 +4,20 @@ import React, { useState } from "react";
 const Todo = () => {
     
 
-    const [items , setitem ] = useState(
-        [
-            {
-                id : 1,
-                task : "Reading books",
-                checked : true,
-            },
-            {
-                id : 2,
-                task : "Making codes",
-                checked : false,
-            },
-            {
-                id : 3,
-                task : "Learn new things",
-                checked : true,
-            },
-            {
-                id : 4,
-                task : "Make exercise",
-                checked : true,
-            }
-    
-        ]
-    
-    )
+    const [items , setItem ] = useState(JSON.parse(localStorage.getItem("todolist")))
 
           function handleDelete (id){
             alert('Are you sure to want to delete this task ?')
             console.log(id);
             const listitems = items.filter(item => item.id !== id);
-            setitem(listitems);
+            setItem(listitems);
             localStorage.setItem("todolist",JSON.stringify(listitems))
           }
     
           function handleCheck (id){
               const listitems = items.map(item => item.id === id ? {...item ,checked: !item.checked} : item)
-              setitem(listitems)
-              localStorage.setitem("todolist",JSON.stringify(listitems))
+              setItem(listitems)
+              localStorage.setItem("todolist",JSON.stringify(listitems))
           }
 
 
@@ -52,31 +27,34 @@ const Todo = () => {
               e.preventDefault();
               console.log("welcome handle submit")
               setnewItem(newItem)
-              console.log(newItem)
               addItem(newItem)
               setnewItem('')
           }
       
-          function addItem (task){
-              console.log(task)
-              const id = items.length ? items[items.length -1].id + 1 : 1
-              console.log(id)
-              const additems =  {id,task,checked:false}
-              const listitems = {...items,additems}
-              console.log(listitems)
-            //   setitem(listitems)
-              // localStorage.setitem("todolist",JSON.stringify(listitems))
-      
+        //   function addItem (task){
+        //       const id = items.length ? items[items.length -1].id + 1 : 1
+        //       const addItems =  {id,task,checked:false}
+        //       const listitems = {...items,addItems}
+        //       setItem(listitems)
+        //       console.log(listitems)
+        //       localStorage.setItem("todolist",JSON.stringify(listitems))
+        //   }
+
+        function addItem() {
+            const id = items.length ? items[items.length - 1].id + 1 : 1;
+            const addItems = { id, task: newItem, checked: false };
+            const listItems = [...items, addItems];
+            setItem(listItems);
+            localStorage.setItem("todolist", JSON.stringify(listItems));
           }
+          
 
-
-  
 
     return(
         <div className=" container mt-20 border border-black mx-auto w-60 p-3">
 
             <h1 className="text-4xl">Todolist</h1>
-            <form className="mt-4">   
+            <form className="mt-4" onSubmit={handleSubmit}>   
                 <label class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                 <div class="relative">
                     
@@ -84,14 +62,15 @@ const Todo = () => {
                     value={newItem}
                     onChange={(e) => setnewItem(e.target.value)}
                     />
-                    <button type="submit" onClick={(e) => handleSubmit(e)} class=" absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">+</button>
+                    <button type="submit" class=" absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">+</button>
                 </div>
             </form>
 
 
 
             <ul >
-              {items.map((item) =>
+              {items.length > 0 ?
+              items.map((item) =>
               (
               
               
@@ -103,7 +82,11 @@ const Todo = () => {
 
               </li>
               
-                ))}
+                ))
+            : (
+                <p className="text-center">Your list is empty</p>
+            )
+            }
 
 
               </ul>
